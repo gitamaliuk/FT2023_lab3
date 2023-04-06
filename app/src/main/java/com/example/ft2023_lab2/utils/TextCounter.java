e9457bfcbbf07d1b63424f8bc869296f3873c38e
Gita <gita.maliukaite12@gmail.com>
2023-04-06 13:19:07 +0300

    Test

--- app/src/main/java/com/example/ft2023_lab2/utils/TextCounter.java
+++ app/src/main/java/com/example/ft2023_lab2/utils/TextCounter.java
@@ -2,6 +2,16 @@ package com.example.ft2023_lab2.utils;
 
 public class TextCounter {
     public static String getCharsCount(String inputString){
-        return String.valueOf(inputString.length());
+        if(inputString != null){
+            return String.valueOf(inputString.length());
+        }
+        return "0";
+    }
+    public static int getWordsCount(String inputString)
+    {
+        String words = inputString.trim();
+        if(words.isEmpty())
+            return 0;
+        return words.split("\\s+").length;
     }
 }
--- app/src/test/java/com/example/ft2023_lab2/ExampleUnitTest.java
+++ app/src/test/java/com/example/ft2023_lab2/ExampleUnitTest.java
@@ -4,14 +4,57 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+import com.example.ft2023_lab2.utils.TextCounter;
+
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void getCharsCount_Given_EmptyString_Result_Zero() {
+        String givenString ="";
+        String expectedValue = "0";
+        String actualValue = TextCounter.getCharsCount(givenString);
+        assertEquals(expectedValue, actualValue);
     }
+    @Test
+    public void getCharsCount_Given_NullString_Result_Zero() {
+        String givenString =null;
+        String expectedValue = "0";
+        String actualValue = TextCounter.getCharsCount(givenString);
+        assertEquals(expectedValue, actualValue);
+    }
+    @Test
+    public void getCharsCount_Given_WhenFiveCharsString_Result_Zero() {
+        String givenString = "12345";
+        String expectedValue = "5";
+        String actualValue = TextCounter.getCharsCount(givenString);
+        assertEquals(expectedValue, actualValue);
+    }
+    @Test
+    public void getWordsCount_EmptyString_ReturnZero() {
+        String givenString = "";
+        int expectedValue = 0;
+        int actualValue = TextCounter.getWordsCount(givenString);
+        assertEquals(expectedValue, actualValue);
+    }
+    @Test
+    public void getWordsCount_EmptySpaces_ReturnZero() {
+        String givenString = "   ";
+        int expectedValue = 0;
+        int actualValue = TextCounter.getWordsCount(givenString);
+        assertEquals(expectedValue, actualValue);
+    }
+    @Test
+    public void getWordsCount_TextString_ReturnNumber() {
+        String givenString = "Hello everyone";
+        int expectedValue = 2;
+        int actualValue = TextCounter.getWordsCount(givenString);
+        assertEquals(expectedValue, actualValue);
+    }
+    @Test
+    public void getWordsCount_SymbolAndSpaces_ReturnNumber(){
+        String givenString = "  :) ?/  *-";
+        int expectedResult = 3;
+        int actualResult = TextCounter.getWordsCount(givenString);
+        assertEquals(expectedResult, actualResult);
+    }
+
 }
